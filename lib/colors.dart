91b9b706ded61c345603b915c1de97dfcb67a82d
saivarshith67 <saivarshith.coder@gmail.com>
2024-02-12 17:54:29 +0530

    Added Colors to colors.dart

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette{
+  Color blue = const Color(0x003abfff);
+  Color lightBlue = const Color(0x00a8edfc);
+  Color darkBlue = const Color(0x002d3eb0);
+  Color green = const Color(0x001b7589);
+  Color orange = const Color(0x00f59e16);
+  Color darkViolet = const Color(0x008a80b4);
+}
