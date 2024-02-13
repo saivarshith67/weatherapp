822378f0b61a4c4f9b55755e4ad35c69e08f599b
saivarshith67 <saivarshith.coder@gmail.com>
2024-02-13 23:11:59 +0530

    Added Fonts and changed home page to HomePage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/colors.dart';
+import 'package:weatherapp/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,41 +15,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'InterRegular'
       ),
       home:  MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  ColorPalette colorPalette = ColorPalette();
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Weather App"),
-        centerTitle: true,
-        backgroundColor: colorPalette.blue.withOpacity(1.0),
-      ),
-      body: Container(
-        color: colorPalette.blue.withOpacity(1.0),
-      )
-
-    );
-  }
-}
